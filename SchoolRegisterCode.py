097968fae2819668877c3ae362b7200b30d85ce6
Basker12 <71788109+Basker12@users.noreply.github.com>
2021-04-19 18:13:21 +0100

    Update SchoolRegisterCode.py

--- SchoolRegisterCode.py
+++ SchoolRegisterCode.py
@@ -6,28 +6,32 @@ class group:
         self.genderIdentify = genderIdentify
 
     def getName(self):
-        self.firstName = input("Please write your name: ")
+        fName = input("Please write your name: ")
+        if fName == fName:
+                firstName = fName.title()
+                self.firstName = firstName
 
     def getLastName(self):
-        self.lastName = input("Please write your last name: ")
+        lName = input("Please write your last name: ")
+        if lName == lName:
+                lastName = lName.title()
+                self.lastName = lastName
 
     def getGender(self):
         print("""1: Male
 2: Female
 3: Other""")
 
-        self.genderIdentify = input("Please write your gender: ")
-        if self.genderIdentify == "Male" or "male" or "M" or "1":
-            genderM = "Male"
-            self.genderIdentify = genderM
+        gender = input("Please write your gender: ")
+        if gender == "Male" or "male" or "M" or "1":
+            gender = self.genderIdentify
             print("Your gender is: ", self.genderIdentify)
 
-        elif self.genderIdentify == "Female" or "female" or "F" or "2":
-            genderF = "Female"
-            self.genderIdentify = genderF
+        elif gender == "Female" or "female" or "F" or "2":
+            gender = self.genderIdentify 
             print("Your gender is: ", self.genderIdentify)
 
-        elif self.genderIdentify == "Other" or "other" or "3":
+        elif gender == "Other" or "other" or "3":
             self.genderIdentify = input("Please write your gender: ")
             print("Your gender is: ", self.genderIdentify)
 
