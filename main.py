73362df70da3c87d7562463204e850a4f09c6d07
Basker12 <71788109+Basker12@users.noreply.github.com>
2021-04-26 12:46:31 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
-def calc():
-    #global age
-    #age = None
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
 
-    print("COVID-19 AGE CALCULATOR")
-    age = int(input("Please enter your age: "))
-    if age <= 20:
-        age = 20
-    if age >= 85:
-        age = 85
+class LoginScreen(GridLayout):
 
-    gender = input("Please write your gender: ").upper()
-    if gender == "Male" or "Female":
-        print("")
+    def __init__(self, **kwargs):
+        super(LoginScreen, self).__init__(**kwargs) #super allows to add functionality
+        self.cols = 1
+
+        self.inside = GridLayout()
+        self.inside.cols = 2 #This function specifies the amount of columns for a element
+
+        self.cols = 2
+        self.inside.add_widget(Label(text="UserName: "))
+        self.username = TextInput(multiline=False)
+        self.inside.add_widget(self.username)
+
+        self.inside.add_widget(Label(text="Password: "))
+        self.password = TextInput(multiline=False)
+        self.inside.add_widget(self.password)
+
+        self.add_widget(self.inside)
+
+        self.submit = Button(text="Submit", font_size=40)
+        self.add_widget(self.submit)
 
 
-calc()
+class MyApp(App):
+    def build(self):
+        self.title = "Qwik Travel LTD"
+        return LoginScreen()
+
+if __name__ == "__main__":
+    MyApp().run()
