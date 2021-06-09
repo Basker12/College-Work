febad45ccf3bb6ff869f3ab621831945dd74ceee
Basker12 <71788109+Basker12@users.noreply.github.com>
2021-06-09 09:29:06 +0100

    [Addition]
    A game funy i made

--- Program.cs
+++ Program.cs
@@ -1,28 +1,87 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
-namespace LinearSearch
+namespace QuikGamingThingy
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] newList = {20, 60, 80, 10, 30, 70, 90, 100 };
-            
-            Console.WriteLine("Please write a number: ");
-            int number = 30;
+            Console.WriteLine("Welcome to this random game that i was asked to make! Please write your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Oh so your name is " + name);
 
-            foreach (var i in newList)
+            Random random = new Random();
+            int randomNumber = random.Next(1, 100);
+
+            int trys = 0;
+
+            Boolean guessing = true;
+            
+            while (guessing)
             {
-                if (number == i)
+                Console.WriteLine("Guess the number: ");
+                Console.WriteLine(randomNumber);
+                
+                int userNum;
+                userNum = int.Parse(Console.ReadLine());
+                
+                if (userNum == randomNumber)
                 {
-                    Console.WriteLine("Your number has been found" + i);
-                    break;
+                    Console.WriteLine("Well done you guessed the random number correctly!");
+                    guessing = false;
                 }
-                else
+
+                if (userNum != randomNumber && userNum > randomNumber) //This checks if the number is too high and if the usernum is not equal too the random number
                 {
-                    Console.WriteLine("Sorry your number hasn't been found.");
+                    Console.WriteLine("Sorry you guess the number too high, try again! \nWrite Quit if you want to exit the program or write Enter if you want to continue: ");
+                    string choice = Console.ReadLine();
+                    
+                    trys += 1;
+                    if (choice == "Enter")
+                    {
+                        Console.WriteLine("");
+                    }
+                    
+                    else if (choice == "Quit" || choice == "quit")
+                    {
+                        System.Environment.Exit(0);
+                    }
+                    
+                    else if (trys == 10)
+                    {
+                        Console.WriteLine("You have run out of trys!");
+                    }
+                }
+
+                if (userNum != randomNumber && userNum < randomNumber)
+                {
+                    Console.WriteLine("Sorry you guessed the number to low, try again! \nWrite Quit if you want to exit the program or write Enter if you want to continue:");
+                    string choice = Console.ReadLine();
+                    
+                    trys += 1;
+                    if (choice == "Enter")
+                    {
+                        Console.WriteLine("");
+                    }
+                    
+                    else if (choice == "Quit" || choice == "quit")
+                    {
+                        System.Environment.Exit(0);
+                    }
+                    
+                    else if (trys == 10)
+                    {
+                        Console.WriteLine("You have run out of trys");
+                        System.Environment.Exit(0);
+                    }
                 }
             }
+        }
+
+        static void HotOrCold()
+        {
+            
         }
     }
 }
