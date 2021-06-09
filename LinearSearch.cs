e828181f6f82b3ac573be9890b0c387871a8d969
Basker12 <71788109+Basker12@users.noreply.github.com>
2021-06-09 09:38:36 +0100

    [Addition]
    A linear search in C#

---
+++ LinearSearch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LinearSearch2
+{
+    class LinearSearch
+    {
+        static void Main(string[] args)
+        {
+            int[] newList = {10, 30, 50, 20, 40, 60, 90, 100};
+            int number = 60;
+
+            foreach (var i in newList)
+            {
+                if (number == i)
+                {
+                    Console.WriteLine("Oh, here is your number " + i);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Still searching...");
+                }
+            }
+            {
+                
+            }
+        }
+    }
+}
