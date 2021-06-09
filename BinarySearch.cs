d03d859766c52628e17905add6ae30e8d9554eb3
Basker12 <71788109+Basker12@users.noreply.github.com>
2021-06-09 10:40:37 +0100

    Add files via upload

---
+++ BinarySearch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Immutable;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new[] {10, 20, 30, 40, 50};
+            var index = BinarySearch(arr, 30);
+            Console.WriteLine("Element found at {0}", index);
+        }
+
+        public static int BinarySearch(int[] arr, int key)
+        {
+            int left = 0;
+            int right = arr.Length;
+            while (left < right)
+            {
+                int mid = (left + right) / 2;
+                if (arr[mid] == key) return mid;
+                else if (arr[mid] > key)
+                {
+                    right = mid;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
