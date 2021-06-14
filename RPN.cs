0e3052648c2fb855b3c56697a476cf8f36861de9
Basker12 <71788109+Basker12@users.noreply.github.com>
2021-06-14 11:07:13 +0100

    [Addition]
    This is Reverse Polish Notation in C#

---
+++ RPN.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace RPN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] contents = {"5", "5", "+", "5", "5", "+", "+" };
+            int results = RPNSolved(contents);
+            Console.WriteLine(results);
+            Console.ReadKey();
+        }
+
+        static int RPNSolved(string[] contents)
+        {
+            var R = new Stack<int>();
+            int val1, val2;
+
+            for (int iCounter = 0; iCounter < contents.Length; iCounter++)
+            {
+                switch (contents[iCounter])
+                {
+                    case "+":
+                        val1 = R.Pop();
+                        val2 = R.Pop();
+                        R.Push(val1 + val2);
+                        break;
+                    
+                    case "-":
+                        val1 = R.Pop();
+                        val2 = R.Pop();
+                        R.Push(val1 - val2);
+                        break;
+                    
+                    case "*":
+                        val1 = R.Pop();
+                        val2 = R.Pop();
+                        R.Push(val1 * val2);
+                        break;
+                    
+                    case "/":
+                        val1 = R.Pop();
+                        val2 = R.Pop();
+                        R.Push(val1 / val2);
+                        break;
+                    
+                    default:
+                        R.Push(Convert.ToInt32(contents[iCounter]));
+                        break;
+
+                }
+            }
+
+            return R.Pop();
+        }
+    }
+}
